a76014dad71d91a4a329536d9d310e97c732e746
IamEld3st <iameld3st@gmail.com>
2017-04-18 20:23:08 +0200

    Added support to show all map instead of only the last played map

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var apiKEY,
-	matchID,
-	lastGame;
+	matchID;
 
 $('#btnUpdate').on('click', function () {
 	console.log("btnUpdate pressed!");
+	$('#content').empty();
 	apiKEY = $('#apiKey').val();
 	matchID = $('#matchID').val();
 	console.log("Set values apiKEY: "+apiKEY+" | matchID: "+matchID);
@@ -12,27 +12,28 @@ $('#btnUpdate').on('click', function () {
 		$.get('https://osu.ppy.sh/api/get_match', { k: apiKEY, mp: matchID })
 			.done(function(response){
 				console.log("Got response!");
-				document.getElementById('resultTable').innerHTML = "<tr><th>Slot</th><th>Score</th><th>Accuracy</th><th>300s</th><th>100s</th><th>50s</th><th>Miss</th><th>Status</th></tr>";
-				lastGame = response.games[response.games.length-1];
-				console.log(response);
 				document.getElementById('matchName').innerHTML = response.match.name;
-				document.getElementById('latestMap').innerHTML = lastGame.beatmap_id;
-				lastGame.scores.forEach(function(item){
-					var passed;
-					if (item.pass == "1") {
-						passed = "PASSED";
-					}else{
-						passed = "FAILED";
+				for (var i = response.games.length - 1; i >= 0; i--) {
+					if (response.games[i].end_time != null) {
+						$("#content").append("<div class=\"row\"><h4>Map<b>ID</b>: "+response.games[i].beatmap_id+"</h4></div><div class=\"row\"><table class=\"table table-condensed\" id=\"table"+i+"\"><tr><th>Slot</th><th>Score</th><th>Accuracy</th><th>300s</th><th>100s</th><th>50s</th><th>Miss</th><th>Status</th></tr></table></div><hr>");
+						response.games[i].scores.forEach(function(item){
+							var passed;
+							if (item.pass == "1") {
+								passed = "PASSED";
+							}else{
+								passed = "FAILED";
+							}
+							var a = parseInt(item.count300);
+							var b = parseInt(item.count100);
+							var c = parseInt(item.count50);
+							var d = parseInt(item.countmiss);
+							var actualSlot = parseInt(item.slot)+1;
+							var accuracy = (a*300+b*100+c*50)/(3*(a+b+c+d));
+							console.log("Calculated accuracy for slot "+ actualSlot +" to be "+accuracy);
+							$("#table"+i).append("<tr><td>"+actualSlot+"</td><td>"+item.score+"</td><td>"+accuracy+"</td><td>"+a+"</td><td>"+b+"</td><td>"+c+"</td><td>"+d+"</td><td>"+passed+"</td></tr>");
+						});
 					}
-					var a = parseInt(item.count300);
-					var b = parseInt(item.count100);
-					var c = parseInt(item.count50);
-					var d = parseInt(item.countmiss);
-					var actualSlot = parseInt(item.slot)+1;
-					var accuracy = (a*300+b*100+c*50)/(3*(a+b+c+d));
-					console.log("Calculated accuracy for slot "+ actualSlot +" to be "+accuracy);
-					document.getElementById('resultTable').innerHTML += "<tr><td>"+actualSlot+"</td><td>"+item.score+"</td><td>"+accuracy+"</td><td>"+a+"</td><td>"+b+"</td><td>"+c+"</td><td>"+d+"</td><td>"+passed+"</td></tr>";
-				});
+				}
 			});
 	}
 });
