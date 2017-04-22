5b9a8b470fbacd86285c3469eb1a648d6d2ba22a
IamEld3st <iameld3st@gmail.com>
2017-04-22 07:21:43 +0200

    Changed the position of the profile links to be the usernames itself

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $('#btnUpdate').on('click', function () {
 				document.getElementById('matchName').innerHTML = response.match.name;
 				for (var i = response.games.length - 1; i >= 0; i--) {
 					if (response.games[i].end_time != null) {
-						$("#content").append("<div class=\"row\"><h4>Map<b>ID</b>: "+response.games[i].beatmap_id+"</h4></div><div class=\"row\"><table class=\"table table-condensed\" id=\"table"+i+"\"><tr><th>Slot</th><th>Username</th><th>Score</th><th>Accuracy</th><th>300s</th><th>100s</th><th>50s</th><th>Miss</th><th>Status</th><th>URLs</th></tr></table></div>");
+						$("#content").append("<div class=\"row\"><h4>Map<b>ID</b>: "+response.games[i].beatmap_id+"</h4></div><div class=\"row\"><table class=\"table table-condensed\" id=\"table"+i+"\"><tr><th>Slot</th><th>Username</th><th>Score</th><th>Accuracy</th><th>300s</th><th>100s</th><th>50s</th><th>Miss</th><th>Status</th></tr></table></div>");
 						response.games[i].scores.forEach(function(item){
 							console.log("Checking if player is in results...");
 							if (!(results.hasOwnProperty(item.user_id))) {
@@ -42,7 +42,7 @@ $('#btnUpdate').on('click', function () {
 							var accuracy = (a*300+b*100+c*50)/(3*(a+b+c+d));
 							console.log("Calculated accuracy for slot "+ actualSlot +" to be "+accuracy);
 							results[item.user_id].accuracyList.push(accuracy);
-							$("#table"+i).append("<tr><td>"+actualSlot+"</td><td><span class=\"user"+item.user_id+"\"></span></td><td>"+item.score+"</td><td>"+accuracy+"</td><td>"+a+"</td><td>"+b+"</td><td>"+c+"</td><td>"+d+"</td><td>"+passed+"</td><td><a href=\"https://osu.ppy.sh/u/"+item.user_id+"\">Profile</a></td></tr>");
+							$("#table"+i).append("<tr><td>"+actualSlot+"</td><td><a href=\"https://osu.ppy.sh/u/"+item.user_id+"\"><span class=\"user"+item.user_id+"\"></span></a></td><td>"+item.score+"</td><td>"+accuracy+"</td><td>"+a+"</td><td>"+b+"</td><td>"+c+"</td><td>"+d+"</td><td>"+passed+"</td></tr>");
 						});
 					}
 				}
@@ -65,7 +65,7 @@ $('#btnUpdate').on('click', function () {
 				// Check if all get requests finished
 				$(document).ajaxStop(function(){
 					console.log("Finished fetching usernames... Calculating top table...");
-					document.getElementById("accuracyData").innerHTML = "<tr><th>Username</th><th>Best Accuracy</th><th>Average Accuracy</th><th>Worst Accuracy</th><th>URLs</th></tr>";
+					document.getElementById("accuracyData").innerHTML = "<tr><th>Username</th><th>Best Accuracy</th><th>Average Accuracy</th><th>Worst Accuracy</th></tr>";
 					Object.keys(results).forEach(function(key,index) {
 						var worst = 100;
 						var best = 0;
@@ -76,7 +76,7 @@ $('#btnUpdate').on('click', function () {
 							average += results[key].accuracyList[i];
 						}
 						average = average / results[key].accuracyList.length;
-						$("#accuracyData").append("<tr><td>"+results[key].username+"</td><td>"+best+"</td><td>"+average+"</td><td>"+worst+"</td><td><a href=\"https://osu.ppy.sh/u/"+key+"\">Profile</a></td></tr>");
+						$("#accuracyData").append("<tr><td><a href=\"https://osu.ppy.sh/u/"+key+"\">"+results[key].username+"</a></td><td>"+best+"</td><td>"+average+"</td><td>"+worst+"</td></tr>");
 						$("span.user"+key).empty();
 						$("span.user"+key).append(results[key].username);
 					});
